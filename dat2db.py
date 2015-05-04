f69596b2febf0dcb700fe1eaa4856931e0879f2b
Patrick Rabu <patrick@rabu.fr>
2015-05-04 16:41:58 +0200

    Restore database statements.

--- dat2db.py
+++ dat2db.py
@@ -3,7 +3,7 @@
 #
 # dat2db - Insert content of dat file from the Voltcraft DL-120TH into a sqlite DB.
 #
-# Copyright 2012 Patrick Rabu
+# Copyright 2012,2015 Patrick Rabu
 
 import argparse
 import sys
@@ -29,10 +29,10 @@ if __name__ == '__main__':
         commandOk = False
     else:
         filename = args.filename
-        datfile = open(args.filename)
 
     if args.database == None:
-        dbname = args.filename
+        print "DataBase name is mandatory."
+        commandOk = False
     else:
         dbname = args.database
 
@@ -40,29 +40,26 @@ if __name__ == '__main__':
         print "Command line error..."
         sys.exit(2)
 
-    #conn = sqlite3.connect(dbname)
+    conn = sqlite3.connect(dbname)
 
-    #c = conn.cursor()
-    #c.execute('''CREATE TABLE IF NOT EXISTS sensors (logger text, dt text, temp real, hygro real)''')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS sensors (logger text, dt text, temp real, hygro real)''')
 
-    datfile = open(filename)
-
-    try:
+    with open(filename) as datfile:
+        # Read the first line
         line = datfile.readline()
         print line
         words = line.split()
         print words[0], "-", words[1], "-", words[2], "-", words[3], "-", words[4]
+        logger_name = words[1]
 
         for line in datfile:
             words = line.split()
-            #print words[0], "-", words[3], "-", words[4]
-            #c.execute('INSERT INTO sensors VALUES (?, ?, ?, ?)', ('rdc', words[0], words[1], words[2]))
+            print words[0], "-", words[3], "-", words[4]
+            c.execute('INSERT INTO sensors VALUES (?, ?, ?, ?)', (logger_name, words[0], words[3], words[4]))
 
-    finally:
-        datfile.close()
-
-    #conn.commit()
-    #conn.close()
+    conn.commit()
+    conn.close()
 
 sys.exit(0)
 
