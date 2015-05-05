af1c1825820ad4bbb05ec66e8a61be2d2f8e1cb2
Patrick Rabu <patrick@rabu.fr>
2015-05-05 23:49:57 +0200

    Pygal trial

--- plotdb.py
+++ plotdb.py
@@ -13,10 +13,7 @@ import locale
 import datetime
 from time import strftime, localtime
 
-from matplotlib import pyplot
-from matplotlib import dates as mdates
-from matplotlib.dates import epoch2num
-
+import pygal
 
 if __name__ == '__main__':
 
@@ -45,7 +42,7 @@ if __name__ == '__main__':
         database = args.database
 
     if args.output is None:
-        output = args.month + '.png'
+        output = args.month + '.svg'
     else:
         output = args.output
 
@@ -66,12 +63,11 @@ if __name__ == '__main__':
     titre = dtmin.strftime("Releves de %B %Y")
     print(titre)
 
+    dates = []
     # first logger
-    dates0 = []
     temp0 = []
     hygro0 = []
     # Second logger
-    dates1 = []
     temp1 = []
     hygro1 = []
 
@@ -86,62 +82,24 @@ if __name__ == '__main__':
     c = conn.cursor()
 
     for row in c.execute(stmt, (dtmin.strftime("%s"), dtmax.strftime("%s"))):
+        dates.append(float(row[1]))
         if row[0] == 'rdc':
-            dates0.append(float(row[1]))
             temp0.append(row[2])
             hygro0.append(row[3])
         else:
-            dates1.append(float(row[1]))
             temp1.append(row[2])
             hygro1.append(row[3])
-        # print row[0].encode('ascii'), ' ', row[1], ' ', row[2], ' ', row[3]
-
-    # print dates
-    # print temp
-    # print hygro
-    # lineChart('plotdb.png', temp, hygro)
 
     conn.close()
 
-    # Matplotlib date format
-    dfmt = mdates.DateFormatter('%d')
-
-    # Creation of the figure 9 inches x 6 inches
-    fig = pyplot.figure(figsize=(9, 6))
-    # fig, (ax0, ax1) = pyplot.subplots(nrows=2, sharex=True)
-
-    # ax0 = fig.add_subplot(111)
-    ax0 = pyplot.subplot(2, 1, 1)
-    # Un trait par jour
-    ax0.xaxis.set_major_locator(mdates.DayLocator())
-    # Un trait toutes les 6 heures
-    ax0.xaxis.set_minor_locator(mdates.HourLocator(interval=6))
-    ax0.xaxis.set_major_formatter(dfmt)
-    ax0.set_xlim(dtmin, dtmax)
-    ax0.set_ylim(10, 35)
-    ax0.plot_date(epoch2num(dates0), temp0, '-', xdate=True)
-    ax0.plot_date(epoch2num(dates1), temp1, '-', xdate=True)
-    ax0.grid(True)
-    # ax0.set_title(dtmin.strftime("Températures de %B %Y"))
-    pyplot.ylabel("Températures")
-    pyplot.title(titre)
-
-    ax1 = pyplot.subplot(2, 1, 2)
-    # Un trait par jour
-    ax1.xaxis.set_major_locator(mdates.DayLocator())
-    # Un trait toutes les 6 heures
-    ax1.xaxis.set_minor_locator(mdates.HourLocator(interval=6))
-    ax1.xaxis.set_major_formatter(dfmt)
-    ax1.set_xlim(dtmin, dtmax)
-    ax1.set_ylim(35, 75)
-    ax1.plot_date(epoch2num(dates0), hygro0, '-', xdate=True)
-    ax1.plot_date(epoch2num(dates1), hygro1, '-', xdate=True)
-    ax1.grid(True)
-    pyplot.ylabel("Hygrométrie")
-    pyplot.xlabel('Jours')
-
-    #fig.set_title(titre)
-    fig.autofmt_xdate()
-    pyplot.savefig(output)
+    chart = pygal.Line(x_label_rotation=20)
+    chart.title = titre
+    chart.x_label_format = '%d'
+    chart.x_labels = map(str, dates)
+    chart.add('Temp RDC', temp0)
+    chart.add('Temp étage', temp1)
+    chart.add('Hygro RDC', hygro0)
+    chart.add('Hygro étage', hygro1)
+    chart.render_to_file(output)
 
 sys.exit(0)
